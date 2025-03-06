dc41669badb080fa9739457523d5c7b205a51997
MD Zubaidur Rahman Bagmar <zubaidur99@gmail.com>
2025-03-06 16:34:47 +0600

    added

--- index.js
+++ index.js
@@ -8,10 +8,25 @@ const router = require('./routes')
 
 const app = express()
 // app.use(cors())
-app.use(cors({
-    origin : process.env.FRONTEND_URL,
-    credentials : true
-}))
+const allowedOrigins = [
+    'http://localhost:5173',
+    'https://primtech.com.bd'
+  ];
+  
+  app.use(cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true
+  }));
+
+
+
+
 app.use(express.json())
 app.use(cookieParser())
 
